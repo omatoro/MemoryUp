543cce7444aafa42766da75fddbc209031fb9a8f
omatoro <okayama@grincode.com>
2013-01-15 12:21:03 +0900

    init.js->game.js
    コミットミス修正

--- js/game.js
+++ js/game.js
@@ -13,22 +13,18 @@ var GAME = GAME || {};
         // デバッグ時定数
         ns.DEBUG = true;
 
-        // 初期化関数
-        ns.init = function() {
+        // アプリケーション作成
+        ns.app = tm.app.CanvasApp("#world");
+        ns.app.resizeWindow(); // 画面サイズに合わせる
+        ns.app.fitWindow(); // リサイズ対応
+        ns.app.background = "rgba(0, 0, 0, 0.1)"; // 背景色をセット
 
-            // アプリケーション作成
-            ns.app = tm.app.CanvasApp("#world");
-            ns.app.resizeWindow(); // 画面サイズに合わせる
-            ns.app.fitWindow(); // リサイズ対応
-            ns.app.background = "rgba(0, 0, 0, 0.1)"; // 背景色をセット
+        // デバッグ時のみ
+        if (this.DEBUG === true) {
+            ns.app.enableStats();
+        }
 
-            // デバッグ時のみ
-            if (this.DEBUG === true) {
-                ns.app.enableStats();
-            }
-
-            ns.app.replaceScene(this.SceneTitle());
-        };
+        ns.app.replaceScene(this.SceneTitle());
 
         // タイトルシーンの生成
         var title = ns.SceneTitle();
