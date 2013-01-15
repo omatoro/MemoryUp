d0978ca2f1fe02533ac060273b80a66e33efa641
omatoro <okayama@grincode.com>
2013-01-15 12:19:15 +0900

    init.js->game.js

--- js/game.js
+++ js/game.js
@@ -4,41 +4,31 @@
 var GAME = GAME || {};
 
 /*
- * ゲーム初期化処理
- */
-(function(ns) {
-
-    // デバッグ時定数
-    ns.DEBUG = true;
-
-    // 初期化関数
-    ns.init = function() {
-
-        // アプリケーション作成
-        ns.app = tm.app.CanvasApp("#world");
-        ns.app.resizeWindow(); // 画面サイズに合わせる
-        ns.app.fitWindow(); // リサイズ対応
-        ns.app.background = "rgba(0, 0, 0, 0.1)"; // 背景色をセット
-
-        // デバッグ時のみ
-        if (this.DEBUG === true) {
-            ns.app.enableStats();
-        }
-
-        ns.app.replaceScene(this.SceneTitle());
-    };
-
-})(GAME);
-
-/*
  * ゲーム起動処理
  */
 (function(ns) {
 
     ns.run = function() {
 
-        // 画面初期化など
-        this.init();
+        // デバッグ時定数
+        ns.DEBUG = true;
+
+        // 初期化関数
+        ns.init = function() {
+
+            // アプリケーション作成
+            ns.app = tm.app.CanvasApp("#world");
+            ns.app.resizeWindow(); // 画面サイズに合わせる
+            ns.app.fitWindow(); // リサイズ対応
+            ns.app.background = "rgba(0, 0, 0, 0.1)"; // 背景色をセット
+
+            // デバッグ時のみ
+            if (this.DEBUG === true) {
+                ns.app.enableStats();
+            }
+
+            ns.app.replaceScene(this.SceneTitle());
+        };
 
         // タイトルシーンの生成
         var title = ns.SceneTitle();
