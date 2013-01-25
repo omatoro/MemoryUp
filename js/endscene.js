5de5c287505e44c9f344d13a9f17f84bbf7d9cb4
omatoro <okayama@grincode.com>
2013-01-25 14:01:08 +0900

    EndSceneクラスの基底クラスをResultSceneに変更
    参考にしたサンプル
    http://jsdo.it/phi/2moF

--- js/endscene.js
+++ js/endscene.js
@@ -39,21 +39,16 @@
             // スコア初期化
             RESULT_PARAM.score = score + " / 10 問正解！";
             this.superInit(RESULT_PARAM);
-
-            // ボタン作成
-//            var title_button = tm.app.iPhoneButton(200, 100, "#222222");
-//            title_button.setPosition(250, 500);
-//            title_button.label.text = "もっかい行っとく？";
-//            this.addChild(title_button);
-//            this.title_button = title_button;
         },
 
         update : function() {
+        },
 
-//            this.title_button.onpointingstart = function () {
-//                ns.app.replaceScene(ns.TitleScene());
-//            };
-        }
+        // Backボタンを押したら、onpointingstart->インスタンス.dispatchEventにより
+        // 以下onnextsceneイベントが実行される
+        onnextscene : function () {
+            ns.app.replaceScene(ns.TitleScene());
+        },
     });
 
 })(game);
